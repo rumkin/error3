8fc24a3718e62cacfac0e4beefd86426bb4294c0
Pavel Rumkin <dev@rumk.in>
2017-07-02 19:25:35 +0300

    [src,tests,utils] add fromJSON method to restore error from JSON; add test; bump version

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ class Error3 extends Error {
         let message;
         let details;
         let errors;
-        
+
         for (let i = 0; i < length; i++) {
             let arg = args[i];
-            
+
             if (hasMessage && hasErrors && hasDetails) {
                 break;
             }
-            
+
             if (typeof arg === 'string') {
                 if (hasMessage) {
                     break;
                 }
-                
+
                 // This is a message
                 message = arg;
                 hasMessage = true;
@@ -30,7 +30,7 @@ class Error3 extends Error {
                 if (hasErrors) {
                     break;
                 }
-                
+
                 errors = arg.slice();
                 hasErrors = true;
                 hasMessage = true;
@@ -40,7 +40,7 @@ class Error3 extends Error {
                 if (hasErrors) {
                     break;
                 }
-                
+
                 errors = [arg];
                 hasErrors = true;
                 hasMessage = true;
@@ -50,7 +50,7 @@ class Error3 extends Error {
                 if (hasDetails) {
                     break;
                 }
-                
+
                 details = arg;
                 hasDetails = true;
                 hasMessage = true;
@@ -59,20 +59,20 @@ class Error3 extends Error {
                 break;
             }
         }
-        
+
         if (! message) {
             message = code.replace(/_/g, ' ');
         }
-        
+
         super(message);
-        
+
         this.name = this.constructor.name;
         this.code = code;
         this.message = message;
         this.details = Object.assign({}, details);
         this.errors = errors || [];
     }
-    
+
     valueOf() {
         return {
             code: this.code,
@@ -80,9 +80,17 @@ class Error3 extends Error {
             details: this.details,
         };
     }
-    
+
     toJSON() {
         return this.valueOf();
+    }
+
+    static fromJSON(value) {
+        const errors = value.errors
+            ? value.errors.map(this.fromJSON.bind(this))
+            : [];
+
+        return new this(value.code, value.message, value.details || {}, errors);
     }
 }
 
--- test/test.spec.js
+++ test/test.spec.js
@@ -11,108 +11,132 @@ describe('Error3', function() {
             should(error).hasOwnProperty('message');
             should(error).hasOwnProperty('details');
             should(error).hasOwnProperty('errors');
-            
+
             should(error.code).be.equal('test_ok');
             should(error.message).be.equal('test ok');
             should(error.details).be.deepEqual({});
-            
+
             should(error.errors).has.lengthOf(0);
         });
-        
+
         it('Should instantiate with code and error', () => {
             let error = new Error3('test_ok', new Error('test'));
             should(error).hasOwnProperty('code');
             should(error).hasOwnProperty('message');
             should(error).hasOwnProperty('details');
             should(error).hasOwnProperty('errors');
-            
+
             should(error.code).be.equal('test_ok');
             should(error.message).be.equal('test ok');
             should(error.details).be.deepEqual({});
-            
+
             should(error.errors).has.lengthOf(1);
             should(error.errors[0]).be.instanceOf(Error);
         });
-        
+
         it('Should instantiate with code and errors', () => {
             let error = new Error3('test_ok', [new Error('test')]);
             should(error).hasOwnProperty('code');
             should(error).hasOwnProperty('message');
             should(error).hasOwnProperty('details');
             should(error).hasOwnProperty('errors');
-            
+
             should(error.code).be.equal('test_ok');
             should(error.message).be.equal('test ok');
             should(error.details).be.deepEqual({});
-            
+
             should(error.errors).has.lengthOf(1);
             should(error.errors[0]).be.instanceOf(Error);
         });
-        
+
         it('Should instantiate with code, details and errors', () => {
             let error = new Error3('test_ok', {a: 1}, [new Error('test')]);
             should(error).hasOwnProperty('code');
             should(error).hasOwnProperty('message');
             should(error).hasOwnProperty('details');
             should(error).hasOwnProperty('errors');
-            
+
             should(error.code).be.equal('test_ok');
             should(error.message).be.equal('test ok');
             should(error.details).be.deepEqual({a: 1});
-            
+
             should(error.errors).has.lengthOf(1);
             should(error.errors[0]).be.instanceOf(Error);
         });
-        
+
         it('Should instantiate with code, message and errors', () => {
             let error = new Error3('test_ok', 'test', [new Error('test')]);
             should(error).hasOwnProperty('code');
             should(error).hasOwnProperty('message');
             should(error).hasOwnProperty('details');
             should(error).hasOwnProperty('errors');
-            
+
             should(error.code).be.equal('test_ok');
             should(error.message).be.equal('test');
             should(error.details).be.deepEqual({});
-            
+
             should(error.errors).has.lengthOf(1);
             should(error.errors[0]).be.instanceOf(Error);
         });
-        
+
         it('Should instantiate with code, message and details', () => {
             let error = new Error3('test_ok', 'test', {a: 1});
             should(error).hasOwnProperty('code');
             should(error).hasOwnProperty('message');
             should(error).hasOwnProperty('details');
             should(error).hasOwnProperty('errors');
-            
+
             should(error.code).be.equal('test_ok');
             should(error.message).be.equal('test');
             should(error.details).be.deepEqual({a: 1});
-            
+
             should(error.errors).has.lengthOf(0);
         });
-        
+
         it('Should instantiate with code, message, details and errors', () => {
             let error = new Error3('test_ok', 'test', {a: 1}, [new Error('test')]);
             should(error).hasOwnProperty('code');
             should(error).hasOwnProperty('message');
             should(error).hasOwnProperty('details');
             should(error).hasOwnProperty('errors');
-            
+
             should(error.code).be.equal('test_ok');
             should(error.message).be.equal('test');
             should(error.details).be.deepEqual({a: 1});
-            
+
             should(error.errors).has.lengthOf(1);
             should(error.errors[0]).be.instanceOf(Error);
         });
     });
-    
+
     it('Should specify error name for extensions', () => {
        class MyError extends Error3 {}
-       
+
        let error = new MyError('test');
        should(error).hasOwnProperty('name', 'MyError');
+    });
+
+    it('Should create error from JSON', () => {
+       const error = Error3.fromJSON({
+           code: 'code',
+           message: 'Message',
+           details: {
+               value: 1,
+           },
+           errors: [{
+               code: 'invalid',
+               message: 'Invalid',
+           }]
+       });
+
+       should(error.code).be.equal('code');
+       should(error.message).be.equal('Message');
+       should(error.details).be.deepEqual({value: 1});
+
+       const {errors} = error;
+       should(errors).be.instanceOf(Array);
+       should(errors).have.lengthOf(1);
+       should(errors[0]).ownProperty('code').equal('invalid');
+       should(errors[0]).ownProperty('message').equal('Invalid');
     });
 });
